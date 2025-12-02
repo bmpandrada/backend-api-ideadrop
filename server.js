ac42293058f4179e07553b1ed00903325b2d2a90
bruce <bmpandrada@gmail.com>
2025-12-02 16:27:22 +0800

    fix: add endpoints get and post req, res

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/cacaodelio", (req, res) => {
+  const data = [
+    { id: 1, product: "cacao bar premium" },
+    { id: 2, product: "cacao bar premium" },
+  ];
+
+  res.json(data);
+});
+
+app.post("/api/cacaodelio", (req, res) => {
+  const { product } = req.body;
+
+  res.send(product);
+});
+
 app.listen(PORT, () => {
-  console.log(`Port connected ${PORT}`);
+  console.log(`You are connected to port ${PORT}`);
 });
